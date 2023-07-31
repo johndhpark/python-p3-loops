7f14fe7dc45a72f928e68b0b56c2c7991395431e
John DH. Park <john.dh.park@icloud.com>
2023-07-30 19:24:37 -0700

    Completed Control Flow: Loops Assignment

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,30 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    count = 10
+
+    while count > 0:
+        print(count)
+        count -= 1
+
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
-
+    int_list = [val * val for val in int_list]
+    
+    return int_list
+        
 def fizzbuzz():
     # code goes here!
-    pass
+    for val in range(1, 101):
+        if val % 3 == 0 and val % 5 == 0:
+            print("FizzBuzz")
+        elif val % 3 == 0:
+            print("Fizz")
+        elif val % 5 == 0:
+            print("Buzz")
+        else:
+            print(val)
+
